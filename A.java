db800b129396a3c4b95cd06fabee8d85db8539cf
MohitKumarPro <71750255+MohitKumarPro@users.noreply.github.com>
2020-10-05 11:47:58 +0530

    Update A.java
    Added a simple easily understandable code for count the words in a string.

--- A.java
+++ A.java
@@ -5,34 +5,15 @@ class A
 {
 	static int wordCount(String str)	
 	{
-		int totalwords=1;
-		
-	//for trimming left spaces.
-
-		int i=0;
-		char ch[]=str.toCharArray();
-		
-		while(ch[i]==' ')
-		{
-			for(int j=0;j<=ch.length-2;j++)
-			{
-				ch[j]=ch[j+1];
-			}
-						
-			
-		}
-		
-	//for counting words after trimming left spaces.
-
-		for(i=0;i<=ch.length-1;i++)
-		{
-			
-			if((ch[i]==' ' && ch[i+1]!=' ' && ch[i+1]!='\t' )||(ch[i]=='\t' && ch[i+1]!='\t' && ch[i+1]!=' ')) 
-			totalwords++;
-			
-		}	
-			
-			return totalwords;
+		 int totalwords = 0;
+	for(int i=0;i<str.length();i++)
+	{
+	    if(str.charAt(i)==' ')
+	    {
+	        totalwords +=1;
+	    }
+	}
+	return totalwords+1;
 			
 
 	}
